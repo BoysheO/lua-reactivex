35db1a944acb26c7b06c9f75ac0a77970cab4a7e
4O4 <pk.pawelo@gmail.com>
2020-10-16 17:24:23 +0200

    Rename stuff to avoid conflicts

---
+++ reactivex/operators/flatten.lua
@@ -0,0 +1,36 @@
+local Observable = require 'reactivex.observable'
+local Subscription = require 'reactivex.subscription'
+local util = require 'reactivex.util'
+
+--- Returns a new Observable that subscribes to the Observables produced by the original and
+-- produces their values.
+-- @returns {Observable}
+function Observable:flatten()
+  return Observable.create(function(observer)
+    local subscriptions = {}
+
+    local function onError(message)
+      return observer:onError(message)
+    end
+
+    local function onNext(observable)
+      local function innerOnNext(...)
+        observer:onNext(...)
+      end
+
+      local subscription = observable:subscribe(innerOnNext, onError, util.noop)
+      subscriptions[#subscriptions + 1] = subscription
+    end
+
+    local function onCompleted()
+      return observer:onCompleted()
+    end
+
+    subscriptions[#subscriptions + 1] = self:subscribe(onNext, onError, onCompleted)
+    return Subscription.create(function ()
+      for i = 1, #subscriptions do
+        subscriptions[i]:unsubscribe()
+      end
+    end)
+  end)
+end
---
+++ reactivex/operators/pack.lua
@@ -0,0 +1,8 @@
+local Observable = require 'reactivex.observable'
+local util = require 'reactivex.util'
+
+--- Returns an Observable that produces the values of the original inside tables.
+-- @returns {Observable}
+function Observable:pack()
+  return self:map(util.pack)
+end

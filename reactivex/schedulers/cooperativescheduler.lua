a649edbfae745dd97765e52001645356e24fe575
4O4 <pk.pawelo@gmail.com>
2020-10-17 00:08:37 +0200

    Fix clash between unschedule() and update() in CooperativeScheduler

--- reactivex/schedulers/cooperativescheduler.lua
+++ reactivex/schedulers/cooperativescheduler.lua
@@ -14,7 +14,9 @@ CooperativeScheduler.__tostring = util.constant('CooperativeScheduler')
 function CooperativeScheduler.create(currentTime)
   local self = {
     tasks = {},
-    currentTime = currentTime or 0
+    currentTime = currentTime or 0,
+    _tasksPendingRemoval = {},
+    _updating = false,
   }
 
   return setmetatable(self, CooperativeScheduler)
@@ -43,45 +45,78 @@ end
 function CooperativeScheduler:unschedule(task)
   for i = 1, #self.tasks do
     if self.tasks[i] == task then
-      table.remove(self.tasks, i)
+      self:_safeRemoveTaskByIndex(i)
+      return
     end
   end
 end
-
 --- Triggers an update of the CooperativeScheduler. The clock will be advanced and the scheduler
 -- will run any coroutines that are due to be run.
 -- @arg {number=0} delta - An amount of time to advance the clock by. It is common to pass in the
 --                         time in seconds or milliseconds elapsed since this function was last
 --                         called.
 function CooperativeScheduler:update(delta)
+  local throwError, errorMsg = false, nil
+
+  self._updating = true
   self.currentTime = self.currentTime + (delta or 0)
 
-  local i = 1
-  while i <= #self.tasks do
-    local task = self.tasks[i]
+  -- This logic has been splitted to two phases in order to avoid table.remove()
+  -- collisions between update() and unschedule().
+  -- Separate "staging area" has been introduced, which basically consists of
+  -- two additional private tables to temporaily keep track of unscheduled
+  -- and dead tasks.
 
-    if self.currentTime >= task.due then
-      local success, delay = coroutine.resume(task.thread)
+  -- Phase 1 - Execute due tasks
+  for i, task in ipairs(self.tasks) do
+    if not self._tasksPendingRemoval[task] then
+      if self.currentTime >= task.due then
+        local success, delay = coroutine.resume(task.thread)
 
-      if coroutine.status(task.thread) == 'dead' then
-        table.remove(self.tasks, i)
-      else
-        task.due = math.max(task.due + (delay or 0), self.currentTime)
-        i = i + 1
+        if coroutine.status(task.thread) == 'dead' then
+          self:_safeRemoveTaskByIndex(i)
+        else
+          task.due = math.max(task.due + (delay or 0), self.currentTime)
+        end
+
+        if not success then
+          throwError = true
+          errorMsg = delay
+        end
       end
-
-      if not success then
-        error(delay)
-      end
-    else
-      i = i + 1
     end
+  end
+
+  self._updating = false
+
+  -- Phase 2 - Commit changes to the tasks queue and clean staging area
+  self:_commitPendingRemovals()
+
+  if throwError then
+    error(errorMsg)
   end
 end
 
 --- Returns whether or not the CooperativeScheduler's queue is empty.
 function CooperativeScheduler:isEmpty()
-  return not next(self.tasks)
+  return #self.tasks == 0
+end
+
+function CooperativeScheduler:_safeRemoveTaskByIndex(i)
+  if self._updating then
+    self._tasksPendingRemoval[self.tasks[i]] = true
+  else
+    table.remove(self.tasks, i)
+  end
+end
+
+function CooperativeScheduler:_commitPendingRemovals()
+  for i = #self.tasks, 1, -1 do
+    if self._tasksPendingRemoval[self.tasks[i]] then
+      self._tasksPendingRemoval[self.tasks[i]] = nil
+      table.remove(self.tasks, i)
+    end
+  end
 end
 
 return CooperativeScheduler

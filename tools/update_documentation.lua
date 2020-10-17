7df41949cecb6fa3f2cb1eeb5213fbe8149e4403
4O4 <pk.pawelo@gmail.com>
2020-10-17 23:37:13 +0200

    Update documentation generator to find files dynamically and ensure proper ordering

--- tools/update_documentation.lua
+++ tools/update_documentation.lua
@@ -1,12 +1,79 @@
-dofile("tools/build.lua")
-
-local docroc = require 'tools/docroc'
+local lfs = require("lfs")
+local docroc = require("tools/docroc")
 
 io.output('doc/README.md')
 
-io.write('Lua-ReactiveX\n===\n\n')
+io.write('# Lua-ReactiveX\n\n')
 
-local comments = docroc.process('.tmp/lua-reactivex-portable/reactivex.lua')
+function readFile(filename)
+  local file = io.open(filename, 'r')
+  local text = file:read('*a')
+  file:close()
+  
+  return text
+end
+
+function getFileListRecursive(path, rules, accumulator)
+  local accumulator = accumulator or {}
+
+  for file in lfs.dir(path) do
+      if file ~= "." and file ~= ".." then
+          local f = path..'/'..file
+          local attr = lfs.attributes (f)
+          assert (type(attr) == "table")
+          if attr.mode == "directory" then
+            getFileListRecursive (f, rules, accumulator)
+          else
+            local excluded = false
+
+            if rules and rules.excludePatterns then
+              for _, pathPattern in ipairs(rules.excludePatterns) do
+                if f:match(pathPattern) then
+                  excluded = true
+                  break
+                end
+              end
+            end
+
+            if not excluded then
+              if rules and rules.includePatterns then
+                for _, pathPattern in ipairs(rules.includePatterns) do
+                  if f:match(pathPattern) then
+                    table.insert(accumulator, f)
+                    break
+                  end
+                end
+              else
+                table.insert(accumulator, f)
+              end
+            end
+          end
+      end
+  end
+
+  return accumulator
+end
+
+function getContents(...)
+  local fileLists = { ... }
+  local contents = {}
+
+  for k, fileList in ipairs(fileLists) do
+    for k,file in ipairs(fileList) do
+      table.insert(contents, readFile(file) or "")
+    end
+  end
+
+  return contents
+end
+
+local observableSources = getFileListRecursive("reactivex", { includePatterns = { "observable%.lua$", "operators/.*%.lua$" } })
+local restOfSources = getFileListRecursive("reactivex", { excludePatterns = { "observable%.lua$", "operators/.*%.lua$" } })
+local allSourceContents = getContents(observableSources, restOfSources)
+local text = table.concat(allSourceContents, "\n\n")
+
+local comments = docroc.process_text(text)
+local lastClass = ""
 
 -- Generate table of contents
 for _, comment in ipairs(comments) do
@@ -14,6 +81,7 @@ for _, comment in ipairs(comments) do
 
   if tags.class then
     local class = tags.class[1].text
+    lastClass = class
     io.write('- [' .. class .. '](#' .. class:lower() .. ')\n')
   else
     local context = comment.context:match('function.-([:%.].+)')
@@ -30,8 +98,9 @@ for _, comment in ipairs(comments) do
     end
 
     local name = comment.context:match('function.-[:%.]([^%(]+)')
+    local functionName = comment.context:match('function.-[:.]([^(]+)')
 
-    io.write('  - [' .. name .. '](#' .. context:gsub('[^%w%s]+', ''):gsub(' ', '-'):lower() .. ')\n')
+    io.write('  - [' .. name .. '](#' .. lastClass:lower() .. "-" .. functionName:lower() .. ')\n')
   end
 end
 
@@ -42,12 +111,15 @@ for _, comment in ipairs(comments) do
   local tags = comment.tags
 
   if tags.class then
-    io.write('# ' .. tags.class[1].text .. '\n\n')
+    lastClass = tags.class[1].text
+    io.write('## ' .. tags.class[1].text .. '\n\n')
     if tags.description then
       io.write(tags.description[1].text .. '\n\n')
     end
   else
     local context = comment.context:match('function.-([:%.].+)')
+    local functionName = context:match('[.:]([^(]+)')
+
     if tags.arg then
       context = context:gsub('%b()', function(signature)
         local args = {}
@@ -61,7 +133,7 @@ for _, comment in ipairs(comments) do
 
     end
 
-    io.write(('---\n\n#### `%s`\n\n'):format(context))
+    io.write(('---\n<a name="' .. lastClass:lower() .. '-' .. functionName:lower() .. '"></a>\n### `%s`\n\n'):format(context))
 
     if tags.description then
       io.write(('%s\n\n'):format(tags.description[1].text))
